2dbe5d89895b9da2106498c1c3d57a674448ce8e
SilentNN <akanam@gmail.com>
2020-10-09 13:46:24 -0700

    add #add_child, #remove_child, #dfs, #bfs methods to PolyTreeNode class

--- intro_data_structures_PolyTreeNode/lib/00_tree_node.rb
+++ intro_data_structures_PolyTreeNode/lib/00_tree_node.rb
@@ -18,6 +18,45 @@ class PolyTreeNode
     end
 
     def add_child(child)
+        child.parent = self
+    end
 
+    def inspect
+        "#{@value} #{@parent} #{@children}"
+    end
+
+    def remove_child(child)
+        raise "Node is not a child" if !self.children.include?(child) 
+        child.parent = nil
+    end
+
+    def dfs(target)
+        return self if self.value == target
+        
+        self.children.each do |child| 
+            result = child.dfs(target)
+            return result unless result.nil? 
+        end
+
+        nil
+    end
+
+    def bfs(target)
+        queue = [self]
+        until queue.empty?
+            cur_node = queue.shift
+            return cur_node if cur_node.value == target
+            queue += cur_node.children
+        end
+        nil
     end
 end
+
+# class Searchable
+#     def dfs(node, target)
+#         return node if node.value == target
+#         self.each do |n|
+#             dfs(node.children)
+#         end
+#     end
+# end
